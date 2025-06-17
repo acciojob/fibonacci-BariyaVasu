9201f7efe814176063fc4a0863c11e6cb9894b19
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-17 21:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function fibonacci(num) {
-// your code here
   const sequence = [0, 1];
   for (let i = 2; i < nums; i++) {
     sequence[i] = sequence[i - 1] + sequence[i - 2];
   }
   return sequence.slice(0, nums);
 }
+
+console.log(fibonacci(10));
 
 module.exports = fibonacci;
